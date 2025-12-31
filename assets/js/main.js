0f5f5bd9edbabb15a73082f7dddd1c562c8119ac
Hoon-Hui <jyhoon88@naver.com>
2025-12-31 14:55:38 +0900

    Implement wedding date validation and redirection
    Added a validation function for wedding date redirection.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
+function validateChk() {
+	if(document.getElementById('wedding').value=="20260411"){
+		location.href = "Hoon-Hui_Memories.html";
+	}
+}
 (function() {
 
 	"use strict";
@@ -170,3 +174,4 @@
 
 
 })();
+
