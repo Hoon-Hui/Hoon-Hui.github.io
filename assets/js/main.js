c062ba6355ed6d31708766fbafcd5dd5eb7e70ef
Hoon-Hui <jyhoon88@naver.com>
2026-01-08 17:23:44 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Eventually by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function() {
 
@@ -138,19 +133,3 @@ function blockPC() {
         throw new Error("PC Access Blocked"); // 이후 스크립트 실행 중단.
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
