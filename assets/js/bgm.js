f5f6a68548b3fcb9697854c0a23918055e3a96a7
Hoon-Hui <jyhoon88@naver.com>
2026-01-12 23:51:48 +0900

    Create bgm.js

---
+++ assets/js/bgm.js
@@ -0,0 +1,26 @@
+function toggleMusic() {					
+					var audio = document.getElementById("myAudio");
+					var gif = document.getElementById("gifMusic");
+					var png = document.getElementById("pngMusic");
+					
+				    if (audio.paused) {
+				        audio.play();
+				        gif.style.display = "block";  // 움직이는 아이콘 보이기
+				        png.style.display = "none";   // 멈춘 아이콘 숨기기
+				    } else {
+				        audio.pause();
+				        gif.style.display = "none";   // 움직이는 아이콘 숨기기
+				        png.style.display = "block";  // 멈춘 아이콘 보이기
+				    }
+				}
+
+				function showToast() {
+					var toast = document.getElementById("toast");
+					toast.classList.add("show");
+			
+					setTimeout(() => {
+						toast.classList.remove("show");
+					}, 3000);
+				}
+				
+				window.addEventListener("load", showToast);
