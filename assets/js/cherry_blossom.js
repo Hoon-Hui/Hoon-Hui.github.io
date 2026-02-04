edd6996206c8f561ddf7ca9372f51cf1d2257d57
Hoon-Hui <jyhoon88@naver.com>
2026-02-04 22:26:42 +0900

    Update cherry_blossom.js

--- assets/js/cherry_blossom.js
+++ assets/js/cherry_blossom.js
@@ -17,7 +17,7 @@ const $petal = $('<span class="petal"></span>');
 // 랜덤 회전 값을 생성하는 함수
 const getRandomRotate = () => {
     const rotateX = 360;
-    const rotateY = Math.random() * 600 - 30;
+    const rotateY = Math.random() * 100 - 30;
     const rotateZ = Math.random() * 120 - 30;
     const translateX = Math.random() * 10 - 5;
     const translateY = Math.random() * 10 - 10;
